5fa3718503d979904212eae96796f2494a7d4fc1
CarlosChasi13 <120142318+CarlosChasi13@users.noreply.github.com>
2024-08-03 21:19:34 -0500

    Crud de Autos

--- lib/View/Proveedor/Proveedor_CRUD.dart
+++ lib/View/Proveedor/Proveedor_CRUD.dart
@@ -1,4 +1,8 @@
+import 'package:autos/View/Proveedor/Agregar_Auto.dart';
+import 'package:autos/View/Proveedor/Editar_Auto.dart';
 import 'package:flutter/material.dart';
+import 'package:autos/Model/AutoModel.dart';
+import 'package:autos/Servicios/Auto_Service.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
@@ -16,43 +20,58 @@ class _ProveedorState extends State<Proveedor> {
       appBar: AppBar(
         title: const Text('CRUD Autos'),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Número de columnas en la cuadrícula
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-          childAspectRatio: 1,
-        ),
-        itemCount: 5,  // Número de autos en la lista
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AutoDetailScreen(index: index),
-                ),
-              );
-            },
-            child: Card(
-              margin: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Image.asset(
-                      'assets/images/buggati.jpg', // Cambia la imagen según el índice
-                      fit: BoxFit.cover,
+      body: FutureBuilder(
+        future: getAuto(), 
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No data available'));
+          } else {
+            List autos = snapshot.data!;
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Número de columnas en la cuadrícula
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+                childAspectRatio: 1,
+              ),
+              itemCount: autos.length, // Número de autos en la lista
+              itemBuilder: (context, index) {
+                Auto auto = autos[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AutoDetailScreen(auto: auto),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Image.asset(
+                            auto.imagePath,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(auto.marca, style: const TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Marca del Auto $index', style: const TextStyle(fontWeight: FontWeight.bold)),
-                  ),
-                ],
-              ),
-            ),
-          );
+                );
+              },
+            );
+          }
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -69,32 +88,32 @@ class _ProveedorState extends State<Proveedor> {
 }
 
 class AutoDetailScreen extends StatelessWidget {
-  final int index;
-
-  const AutoDetailScreen({required this.index});
+  final Auto auto;
+  
+  const AutoDetailScreen({required this.auto});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalle del Auto $index'),
+        title: Text('Detalle del ${auto.marca}'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset('assets/images/buggati.jpg', width: double.infinity, height: 200, fit: BoxFit.cover),
+            Image.asset(auto.imagePath, width: double.infinity, height: 200, fit: BoxFit.cover),
             const SizedBox(height: 10),
-            Text('Marca del Auto $index (Año)', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            Text('Marca: ${auto.marca}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             const SizedBox(height: 5),
-            const Text('Empresa'),
+            Text('Empresa: ${auto.empresa}'),
             const SizedBox(height: 5),
-            Text('Descripción del auto $index'),
+            Text('Descripción: ${auto.descripcion}'),
             const SizedBox(height: 5),
-            Text('Características de la empresa $index'),
+            Text('Características: ${auto.caracteristicas}'),
             const SizedBox(height: 5),
-            const Text('Precio: \$XX'),
+            Text('Precio: \$${auto.precio}'),
             const SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -104,14 +123,43 @@ class AutoDetailScreen extends StatelessWidget {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => EditarAuto(index: index)),
+                      MaterialPageRoute(builder: (context) => EditarAuto(auto: auto)),
                     );
                   },
                 ),
                 IconButton(
                   icon: const Icon(Icons.delete),
                   onPressed: () {
-                    // Acción de eliminar
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Confirmar eliminación"),
+                          content: const Text(
+                              "¿Estás seguro de que deseas eliminar este auto?"),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pop(); 
+                              },
+                              child: const Text("Cancelar"),
+                            ),
+                            TextButton(
+                              onPressed: () async {
+                                await eliminarAuto(auto.id).then((_){
+                        Navigator.pop(context);
+                      });
+                                //print("Auto eliminado");
+                                Navigator.of(context)
+                                    .pop(); 
+                              },
+                              child: const Text("Eliminar"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
                   },
                 ),
               ],
@@ -123,286 +171,5 @@ class AutoDetailScreen extends StatelessWidget {
   }
 }
 
-class AgregarAuto extends StatefulWidget {
-  const AgregarAuto({super.key});
 
-  @override
-  _AgregarAutoState createState() => _AgregarAutoState();
-}
 
-class _AgregarAutoState extends State<AgregarAuto> {
-  final _formKey = GlobalKey<FormState>();
-  File? _imageFile;
-
-  _selectImage(ImageSource source) async {
-    ImagePicker picker = ImagePicker();
-    XFile? image = await picker.pickImage(source: source);
-    if (image != null) {
-      setState(() {
-        _imageFile = File(image.path);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Agregar Auto'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Marca del Auto'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la marca del auto';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Año'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el año';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Empresa'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la empresa';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Descripción'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la descripción';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Características'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese las características';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Precio'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el precio';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              _imageFile == null
-                  ? const Text('No se ha seleccionado una imagen.')
-                  : Image.file(_imageFile!, height: 200),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _selectImage(ImageSource.gallery),
-                    child: const Text('Subir Imagen'),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () => _selectImage(ImageSource.camera),
-                    child: const Text('Tomar Foto'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Guardar auto
-                      }
-                    },
-                    child: const Text('Guardar'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Cancelar', style: TextStyle(color: Colors.white)),
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class EditarAuto extends StatefulWidget {
-  final int index;
-
-  const EditarAuto({required this.index});
-
-  @override
-  _EditarAutoState createState() => _EditarAutoState();
-}
-
-class _EditarAutoState extends State<EditarAuto> {
-  final _formKey = GlobalKey<FormState>();
-  File? _imageFile;
-
-  _selectImage(ImageSource source) async {
-    ImagePicker picker = ImagePicker();
-    XFile? image = await picker.pickImage(source: source);
-    if (image != null) {
-      setState(() {
-        _imageFile = File(image.path);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Editar Auto ${widget.index}'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Marca del Auto'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la marca del auto';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Año'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el año';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Empresa'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la empresa';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Descripción'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la descripción';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Características'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese las características';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Precio'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el precio';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              _imageFile == null
-                  ? const Text('No se ha seleccionado una imagen.')
-                  : Image.file(_imageFile!, height: 200),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _selectImage(ImageSource.gallery),
-                    child: const Text('Subir Imagen'),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () => _selectImage(ImageSource.camera),
-                    child: const Text('Tomar Foto'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Guardar cambios del auto
-                      }
-                    },
-                    child: const Text('Guardar'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Cancelar', style: TextStyle(color: Colors.white)),
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-void main() {
-  runApp(const MaterialApp(
-    home: Proveedor(),
-  ));
-}
